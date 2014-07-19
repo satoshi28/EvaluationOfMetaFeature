7f83d9f7df3152efc74c0b9995f1736976b1f2ad
satoshi28 <wonky.workspace@gmail.com>
2014-07-19 12:08:35 +0900

    カメラ画像による類似画像検索

--- EvaluationOfMetaFeature/main.cpp
+++ EvaluationOfMetaFeature/main.cpp
@@ -8,7 +8,7 @@
 
 
 bool readImages(std::vector<std::string> filenames, std::vector<cv::Mat>& images);
-int getResult(std::vector<std::string> filelist, cv::Mat& resultImage);
+int getResult(cv::Mat cameraImage, std::vector<std::string> filelist, cv::Mat& resultImage);
 
 int main()
 {
@@ -37,38 +37,39 @@ int main()
 
 	//データベースから特徴量の読み込み
 	db.loadDB(-1, -1, patterns);
-	
 	PatternDetector patternDetector(patterns);
 
-	std::cout << "start" << std::endl;
+	cv::VideoCapture cap(0);
+	if(!cap.isOpened())  // 成功したかどうかをチェック
+        return -1;
 
-	for(int i = 0; i < queryImages.size(); i++)
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+
+	cv::Mat matchingResult;
+
+	while(1)
 	{
+		cv::Mat frame;
+		cap >> frame;
+
+
 		std::vector<int> ranking;
 
-		patternDetector.findPattern(queryImages[i], ranking);
+		patternDetector.findPattern(frame, ranking);
 		
-		cv::Mat matchingResult;
 		std::vector<std::string> matchingList;
-		matchingList.push_back(backfilelist[i]);
-		//3位まで結果を出す
-		for(int i = 0; i < 3; i++)
-			matchingList.push_back(databaseFilelist[ranking[i]]);
+		matchingList.push_back(databaseFilelist[ranking[0]]);
 		
-		//結果をテキストファイルに出力
-		txtFile << matchingList[0] << "	" << matchingList[1] << std::endl;
-		//結果を画像で出力
-		getResult(matchingList, matchingResult);
+		getResult(frame, matchingList, matchingResult);
+
+		cv::imshow("result",matchingResult);
+
+		cv::waitKey(10);
 		
-		static int count = 0;
-		std::stringstream ss;
-		ss << count;
-		std::string result = "result";
-		result +=  ss.str();
-		result += ".jpg";
-		cv::imwrite(result,matchingResult);
-		count++;
-		
+
+		//std::cout << databaseFilelist[ranking[0]] << std::endl;
 	}
 
 	std::cout << "stop" << std::endl;
@@ -100,28 +101,31 @@ bool readImages(std::vector<std::string> filenames, std::vector<cv::Mat>& images
 	}
 }
 
-int getResult(std::vector<std::string> filelist, cv::Mat& resultImage)
+int getResult(cv::Mat cameraImage, std::vector<std::string> filelist, cv::Mat& resultImage)
 {
 	int total_width =0;
 	int height = 0;
 
 	std::vector<cv::Mat> images;
-	for(int i = 0; i < filelist.size(); i++)
+
+	images.push_back(cameraImage);
+
+	total_width += cameraImage.cols;
+	height = cameraImage.rows;
+
+	cv::Mat image;
+	image = cv::imread(filelist[0],1);			//画像の読み込み
+	if (image.empty())
 	{
-
-		cv::Mat image;
-		image = cv::imread(filelist[i],1);			//画像の読み込み
-		if (image.empty())
-		{
-			std::cout << "Input image cannot be read" << std::endl;
-			return false;
-		}
-		//画像を追加
-		images.push_back(image);
-
-		total_width += image.cols;
-		height = image.rows;
+		std::cout << "Input image cannot be read" << std::endl;
+		return false;
 	}
+	//画像を追加
+	images.push_back(image);
+
+	total_width += image.cols;
+	height = image.rows;
+
 
 	cv::Mat combinedImage(cv::Size(total_width, height), CV_8UC3);
 
